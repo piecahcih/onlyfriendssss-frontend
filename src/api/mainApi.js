026cc38b69643434da0a4a4b4b8079577d081bb8
ben-phattharaphon <phattharaphon.spk@gmail.com>
2026-04-12 00:26:58 +0700

    updateapi

--- src/api/mainApi.js
+++ src/api/mainApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import useUserStore from "../stores/userStore";
 
 export const mainApi = axios.create({
-  baseURL: "http://localhost:3999",
+  baseURL: "http://localhost:3999/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,8 +10,21 @@ export const mainApi = axios.create({
 
 mainApi.interceptors.request.use((config) => {
   const token = useUserStore.getState().token;
-  if (token) {
+  if (token && !config.headers.Authorization) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+export const registerApi = (body) => mainApi.post("/auth/register", body);
+export const loginApi = (body) => mainApi.post("/auth/login", body);
+export const googleLoginApi = (idToken) =>
+  mainApi.post(
+    "/auth/registerOrLogin",
+    {},
+    {
+      headers: { Authorization: `Bearer ${idToken}` },
+    },
+  );
+
+export default mainApi;
