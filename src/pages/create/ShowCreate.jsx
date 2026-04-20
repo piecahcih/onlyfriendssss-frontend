54f76e3c44a72e3b933af5bbad74c0d84c04015b
murthehelp24 <attachaijumpahom@gmail.com>
2026-04-20 21:32:53 +0700

    edit/welcome

--- src/pages/create/ShowCreate.jsx
+++ src/pages/create/ShowCreate.jsx
@@ -12,68 +12,64 @@ import { format } from 'date-fns'
 const TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function ShowCreate() {
-  const navigate = useNavigate()
-  const [isCreating, setIsCreating] = useState(false)
+    const navigate = useNavigate()
+    const [isCreating, setIsCreating] = useState(false)
 
-  const hdlGoBack = () => {
-    navigate(-1)
-  }
-
-  const creatingActivity = useActivityStore(st => st.creatingActivity)
-  // console.log('creatingActivity', creatingActivity)
-
-
-  const hdlCreateActivity = async (e) => {
-    e.preventDefault()
-    setIsCreating(true)
-    try {
-      const formData = new FormData();
-
-      formData.append("title", creatingActivity.title);
-      formData.append("description", creatingActivity.description);
-      formData.append("category", creatingActivity.category);
-      formData.append("placeId", creatingActivity.placeId);
-      formData.append("placeName", creatingActivity.placeName || "");
-      formData.append("address", creatingActivity.address || "");
-      formData.append("latitude", creatingActivity.latitude);
-      formData.append("longitude", creatingActivity.longitude);
-      formData.append("isPublic", creatingActivity.isPublic);
-      formData.append("eventStartTime", new Date(creatingActivity.eventStartTime).toISOString());
-
-      if (creatingActivity.eventEndTime) {
-        formData.append("eventStartTime", new Date(creatingActivity.eventStartTime).toISOString());
-      }
-
-      if (creatingActivity.coverPhoto) {
-        formData.append("coverPhoto", creatingActivity.coverPhoto);
-      }
-
-      await useActivityStore.getState().createActivity(formData)
-
-      navigate('/lddiscover')
-      Swal.fire({
-        title: '<h2 class="text-[24px] font-bold text-neutral leading-tight">Activity Created Successfully</h2>',
-        confirmButtonColor: "#FC5100",
-        width: '300px',
-        padding: '1em',
-      });
-
-    } catch (error) {
-      setIsCreating(false);
-      // ดูว่า Backend ตอบกลับมาว่าอะไร (เช่น 400 Bad Request เพราะ Field ไหนขาด)
-      console.log("Server Response:", error.response?.data);
-      console.error("Full Error:", error);
-
-      const errMsg = error.response?.data?.error || error.response?.data?.message || error.message;
-      alert(errMsg); // พ่น error ออกมาดูสดๆ
+    const hdlGoBack = () => {
+        navigate(-1)
     }
+    
+    const creatingActivity = useActivityStore(st=>st.creatingActivity)
+    // console.log('creatingActivity', creatingActivity)
 
-  }
 
-  const [groupStatus, setGroupStatus] = useState(creatingActivity.isPublic)
-  const hdlPrivacyStatus = () => {
-    // setGroupStatus(creatingActivity.isPublic)
-  }
+const hdlCreateActivity = async (e) => {
+      e.preventDefault()
+      setIsCreating(true)
+      try {
+        const formData = new FormData();
+
+        formData.append("title", creatingActivity.title);
+        formData.append("description", creatingActivity.description);
+        formData.append("category", creatingActivity.category);
+        formData.append("placeId", creatingActivity.placeId);
+        formData.append("placeName", creatingActivity.placeName || "");
+        formData.append("address", creatingActivity.address || "");
+        formData.append("latitude", creatingActivity.latitude);
+        formData.append("longitude", creatingActivity.longitude);
+        formData.append("isPublic", creatingActivity.isPublic);
+        formData.append("eventStartTime", new Date(creatingActivity.eventStartTime).toISOString());
+
+        if (creatingActivity.eventEndTime) {
+            formData.append("eventEndTime", new Date(creatingActivity.eventEndTime).toISOString());
+        }
+
+        if (creatingActivity.maxParticipants) {
+            formData.append("maxParticipants", creatingActivity.maxParticipants);
+        }
+
+        if (creatingActivity.coverPhoto) {
+            formData.append("coverPhoto", creatingActivity.coverPhoto); 
+        }
+
+        await useActivityStore.getState().createActivity(formData)
+
+        navigate('/')
+        Swal.fire({
+          title: '<h2 class="text-[24px] font-bold text-neutral leading-tight">Activity Created Successfully</h2>',
+          confirmButtonColor: "#FC5100",
+          width: '300px',  
+          padding: '1em',  
+        });
+        
+      } catch (error) {
+        setIsCreating(false)
+        console.error(error)
+      }
+
+    }
+    
+    const [groupStatus, setGroupStatus] = useState(creatingActivity.isPublic)
 
   const categoryList = [
     { id: "HEALTH", title: "Health & Wellness", icon: "💪" },
@@ -82,49 +78,49 @@ function ShowCreate() {
     { id: "FOOD", title: "Foodies", icon: "🍱" },
     { id: "TRAVEL", title: "Travel", icon: "✈️" },
   ];
-  const selectedCategory = categoryList.find(cat => cat.id === creatingActivity.category)
+    const selectedCategory = categoryList.find(cat => cat.id === creatingActivity.category)
 
 
   return (
     <div className="min-h-screen bg-base-200 text-neutral">
-
+        
       {/* TopAppBar */}
-      <header className="w-full top-0 sticky z-40 bg-base-200 shadow-[0_8px_32px_rgba(78,33,32,0.08)] flex items-center justify-between px-6 py-4 relative">
+    <header className="w-full top-0 sticky z-40 bg-base-200 shadow-[0_8px_32px_rgba(78,33,32,0.08)] flex items-center justify-between px-6 py-4 relative">
 
-        <button type='button' onClick={() => hdlGoBack()}
-          className="text-[#a83100] hover:opacity-80 transition-opacity active:scale-95 transition-transform duration-200 relative z-10">
-          <LeftIcon className='w-8' />
+        <button type='button' onClick={()=>hdlGoBack()} 
+            className="text-[#a83100] hover:opacity-80 transition-opacity active:scale-95 transition-transform duration-200 relative z-10">
+            <LeftIcon className='w-8' />
         </button>
 
         <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 tracking-[-0.02em] font-bold text-[20px] whitespace-nowrap">
-          Preview Activity
+            Preview Activity
         </h1>
 
         <div className="w-8"></div>
-      </header>
+    </header>
 
       <div className="mx-auto px-6 pt-4 space-y-8">
 
         {/* Form Fields */}
-        <form onSubmit={(e) => hdlCreateActivity(e)}>
+        <form onSubmit={(e)=>hdlCreateActivity(e)}>
 
           <div className="flex gap-3 pb-4">
             {/* Public Badge / Privacy */}
-            <button type='button' onClick={() => hdlPrivacyStatus()}>
-              {groupStatus === true ?
-                <div className="flex items-center justify-between text-[14px] px-3 py-1 rounded-full bg-secondary w-fit">
-                  <div className="flex items-center gap-2">
-                    <span className="text-[18px]">🌎</span>
-                    <p className="font-bold text-white">Public</p>
-                  </div>
-                </div>
-                : <div className="flex items-center justify-between text-[14px] px-3 py-1 rounded-full bg-[#bf2802] w-fit">
-                  <div className="flex items-center gap-2">
-                    <span className="text-[18px]">🔒</span>
-                    <p className="font-bold text-white">Private</p>
-                  </div>
-                </div>}
-            </button>
+              <button type='button' disabled>
+                  {groupStatus === true ? 
+                      <div className="flex items-center justify-between text-[14px] px-3 py-1 rounded-full bg-secondary w-fit">                        
+                          <div className="flex items-center gap-2">
+                              <span className="text-[18px]">🌎</span>
+                              <p className="font-bold text-white">Public</p>
+                          </div>
+                      </div>
+                  :   <div className="flex items-center justify-between text-[14px] px-3 py-1 rounded-full bg-[#bf2802] w-fit">                        
+                          <div className="flex items-center gap-2">
+                              <span className="text-[18px]">🔒</span>
+                              <p className="font-bold text-white">Private</p>
+                          </div>
+                      </div>}
+              </button>
 
             {/* Category */}
             <h3 className="px-4 py-1.5 w-fit rounded-3xl text-[14px] font-medium flex items-center gap-2 border border-secondary text-neutral shadow-md">
@@ -137,6 +133,7 @@ function ShowCreate() {
             <img src={creatingActivity.blob} alt="activityIMG" className='w-full object-contain' />
           </div>
 
+
           {/* Activity Name */}
           <div className="mt-2">
             <label className="text-[22px] font-bold text-neutral">
@@ -144,18 +141,24 @@ function ShowCreate() {
             </label>
           </div>
 
+
           {/* Date & Time Row */}
           <h3 className="">
             <span className=" text-xl">📅</span> {format(new Date(creatingActivity.eventStartTime), 'eee, dd MMM yyyy, HH:mm')}
-            {creatingActivity.eventEndTime && (format(new Date(creatingActivity.eventEndTime), ' - HH:mm'))}
+            {creatingActivity.eventEndTime && (format(new Date(creatingActivity.eventEndTime), ' - eee, dd MMM yyyy, HH:mm'))}
           </h3>
 
-
+          
           {/* Notes */}
           <p className="font-light my-5 leading-relaxed">{creatingActivity.description}</p>
 
+          {/* maxParticipants */}
+          <p className="font-light my-5 leading-relaxed">
+             {creatingActivity.maxParticipants && (`Max ${creatingActivity.maxParticipants} Participants`)} 
+          </p>
+
           {/* Location Section */}
-          <div className="space-y-4">
+          <div className="space-y-4 mt-5">
             <div className="flex items-start gap-3">
               <span className="text-2xl">📍</span>
               <div className="flex flex-col">
@@ -168,7 +171,7 @@ function ShowCreate() {
               </div>
             </div>
 
-            <a
+            <a 
               href={`https://www.google.com/maps/search/?api=1&query=${creatingActivity.latitude},${creatingActivity.longitude}`}
               target="_blank"
               rel="noopener noreferrer"
@@ -204,7 +207,7 @@ function ShowCreate() {
 
           <div className="py-8">
             <button disabled={isCreating}
-              className="w-full py-4 rounded-full bg-linear-to-r from-primary to-secondary text-white font-bold text-lg shadow-[0_8px_32px_rgba(168,49,0,0.24)] active:scale-95 transition-all hover:scale-[1.05]">
+            className="w-full py-4 rounded-full bg-linear-to-r from-primary to-secondary text-white font-bold text-lg shadow-[0_8px_32px_rgba(168,49,0,0.24)] active:scale-95 transition-all hover:scale-[1.05]">
               Create Activity {isCreating && <span className="loading loading-dots loading-md"></span>}
             </button>
           </div>
