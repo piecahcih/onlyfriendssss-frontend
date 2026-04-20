31f4c59a0e9c03b64adcd6f50ca2c910ca9c4b95
piecahcih <pichayapa.thai@gmail.com>
2026-04-20 11:00:43 +0700

    tuja

--- src/components/profile/MyActivityTab.jsx
+++ src/components/profile/MyActivityTab.jsx
@@ -172,10 +172,6 @@ function MyActivityTab() {
                         </div>
 
                         <Wishlist activityId={activity.id} />
-                        {/* <motion.button whileTap={{ scale: 1.2, transition: { duration: 0.2 } }} onClick={hdlLikeClick} className="absolute bottom-4 right-4 p-2 rounded-full bg-white text-primary shadow-lg active:scale-90 transition-transform">
-                          {haveLike ? <HeartIcon className="h-[28px] ml-auto" />
-                            : <HeartLineIcon className="h-[28px] ml-auto text-neutral opacity-80" />}
-                        </motion.button> */}
 
                       </div>
 
@@ -251,10 +247,6 @@ function MyActivityTab() {
 
 
                         <Wishlist activityId={activity.id} />
-                        {/* <motion.button whileTap={{ scale: 1.2, transition: { duration: 0.2 } }} onClick={hdlLikeClick} className="absolute bottom-4 right-4 p-2 rounded-full bg-white text-primary shadow-lg active:scale-90 transition-transform">
-                          {haveLike ? <HeartIcon className="h-[28px] ml-auto" />
-                            : <HeartLineIcon className="h-[28px] ml-auto text-neutral opacity-80" />}
-                        </motion.button> */}
 
                       </div>
 
